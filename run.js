b55b0b7eee7933bc1f7caa7f4110b3db9ff38215
Artem Vasilev <barkqaqa@gmail.com>
2022-12-09 18:32:10 +0300

    run

--- run.js
+++ run.js
@@ -4,9 +4,9 @@ const Game = require('./src/Game');
 
 // Инициализация игры с настройками.
 const game = new Game({
-  trackLength: 30,
+  trackWidth: 30,
+  trackHeight: 6,
 });
-
 
 // Запуск игры.
 game.play();
