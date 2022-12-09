ca099ffdd9efe3b37753b1e431882ca392e81b2d
Tanya <alisaeva4498@mail.ru>
2022-12-09 13:29:20 +0300

    keyboard is done

--- src/keyboard.js
+++ src/keyboard.js
@@ -3,28 +3,28 @@
 // Главное не используй всё вместе!
 
 const keypress = require('keypress');
-
 // Управление.
 // Настроим соответствия нажатий на клавиши и действий в игре.
 
 const keyboard = {
-  q: () => console.log('q'),
-  w: () => console.log('w'),
-  e: () => console.log('e'),
-  r: () => console.log('r'),
-  t: () => console.log('t'),
-  y: () => console.log('y'),
+  left: (hero) => hero.moveLeft(),
+  right: (hero) => hero.moveRight(),
+  down: (hero) => hero.moveDown(),
+  up: (hero) => hero.moveUp(),
+  space: (hero) => hero.attack(),
+  // t: () => console.log('t'),
+  // y: () => console.log('y'),
 };
 
 // Какая-то функция.
 
-function runInteractiveConsole() {
+function runInteractiveConsole(hero) {
   keypress(process.stdin);
   process.stdin.on('keypress', (ch, key) => {
     if (key) {
       // Вызывает команду, соответствующую нажатой кнопке.
       if (key.name in keyboard) {
-        keyboard[key.name]();
+        keyboard[key.name](hero);
       }
       // Прерывание программы.
       if (key.ctrl && key.name === 'c') {
@@ -37,4 +37,4 @@ function runInteractiveConsole() {
 
 // Давай попробуем запустить этот скрипт!
 
-runInteractiveConsole();
+module.exports = runInteractiveConsole;
