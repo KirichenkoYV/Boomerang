768e4e7e8f9a39dc9b708b9c147f5696ea229ef3
Tanya <alisaeva4498@mail.ru>
2022-12-09 18:41:41 +0300

    right and left moves

--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -1,14 +1,27 @@
 // Наш герой.
-const Game = require('../Game');
+const Game = require("../Game");
 
-const player = require('play-sound')(opts = {});
+const player = require("play-sound")((opts = {}));
 
 class Hero {
-  constructor({ boomerang, score = 0, position = { x: 0, y: 0} }) {
-    this.skin = '🤠';
+  constructor({ boomerang, score = 0, position = { x: 0, y: 0 } }) {
+    this.skin = "🤠";
     this.position = position;
     this.hasBoomerang = true;
     this.score = score;
   }
 
+  moveLeft() {
+    // Идём влево.
+    if (this.position.x > 0) {
+      this.position.x -= 1;
+      this.boomerang.position.x -= 1;
+    }
+  }
+
+  moveRight() {
+    // Идём вправо.
+    this.position.x += 1;
+    this.boomerang.position.x += 1;
+  }
 }
